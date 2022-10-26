39905ec9d6dc88fb5079296e1c342ebff2f826f8
Hazem-Farhati <hazemfarhati@gmail.com>
2022-10-26 16:13:44 +0100

    images

--- src/components/DataWork.jsx
+++ src/components/DataWork.jsx
@@ -2,35 +2,35 @@
 export const projectsData = [
   {
     id: 1,
-    image: "/assets/work1.jpg",
+    image: "/assets/lavageimg.jpg",
     title: "Lavage",
     category: "web",
-    video: "/assets/camping.mp4",
+    video: "/assets/lavagee.mp4",
   },
   {
     id: 2,
-    image: "/assets/work2.jpg",
+    image: "/assets/camp.jpg",
     title: "Camping",
     category: "web",
     video: "/assets/camping.mp4",
   },
   {
     id: 3,
-    image: "/assets/work3.jpg",
+    image: "/assets/gym.jpg",
     title: "Gym",
     category: "web",
     video: "/assets/gym.mp4",
   },
   {
     id: 4,
-    image: "/assets/work5.jpg",
+    image: "/assets/movie.jpg",
     title: "Movie App",
     category: "web",
     video: "/assets/movie.mp4",
   },
   {
     id: 5,
-    image: "/assets/work5.jpg",
+    image: "/assets/todo.jpg",
     title: "Todo",
     category: "web",
     video: "/assets/todo.mp4",
@@ -38,7 +38,7 @@ export const projectsData = [
 
   {
     id: 6,
-    image: "/assets/work4.jpg",
+    image: "/assets/gymmobile.jpg",
     title: "gym app",
     category: "app",
     video: "/assets/gym_mobile.mp4",
@@ -55,8 +55,6 @@ export const projectsNav = [
   {
     name: "app",
   },
-  {
-    name: "design",
-  },
+
 ];
 
